293e65a6051f12caabf7b57f9e4c9055c276775c
Christoph Froehlich <christoph.froehlich@spaceteam.at>
2023-01-15 18:18:56 +0000

    Add clang-format-10 install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -14,3 +14,11 @@ rosdep install -riy --from-paths src
 (cd src/ros2_control_demos && pre-commit install)
 (cd src/control_toolbox && pre-commit install)
 (cd src/control_msgs && pre-commit install)
+# install clang-format-10 (old pre-commit hooks)
+curl -LO http://archive.ubuntu.com/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-8_amd64.deb
+sudo dpkg -i libffi6_3.2.1-8_amd64.deb
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+echo 'deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main' | sudo tee -a /etc/apt/sources.list
+echo 'deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-10 main' | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install -y clang-format-10
