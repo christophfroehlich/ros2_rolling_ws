893386051012e1c94015bceb41be880f5eff1b45
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-10-02 20:09:33 +0200

    Fix GPL and sphinx installation

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,13 @@ rosdep update --rosdistro $ROS_DISTRO
 source /opt/ros/rolling/setup.bash
 set -xeu
 vcs import src < ros_control.repos
+# install stuff for control.ros.org
+(cd src/control.ros.org/ && python3 -m pip install -r requirements.txt)
+# Install generate_parameter_library as python package
+# python3 -m pip install pyyaml
+# pip3 install typeguard==4.0.0
+(cd src/generate_parameter_library/generate_parameter_library_py/ && python3 -m pip install .)
+# install other dependencies
 rosdep install -riy --from-paths src
 # if the repo is cloned from windows
 # git config --global --add safe.directory /workspaces/ros2_rolling_ws
@@ -19,9 +26,5 @@ touch src/control.ros.org/COLCON_IGNORE
 echo "*.pyc" > ~/.gitignore
 echo "*__pycache__*" >> ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
-# Install generate_parameter_library as python package
-cd src/generate_parameter_library/generate_parameter_library_py/
-python3 -m pip install pyyaml
-python3 -m pip install .
 #
-echo "alias gitprune='git fetch --prune fork && git fetch --prune origin && git removed-branches --prune --force -r fork && git removed-branches --prune --force -r fork" >> ~/.bashrc
+echo "alias gitprune='git fetch --prune fork && git fetch --prune origin && git removed-branches --prune --force -r fork && git removed-branches --prune --force -r fork'" >> ~/.bashrc
