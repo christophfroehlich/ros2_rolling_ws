d8895fa56c08b6a7351ce621fec94262cb4938fe
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-03-02 17:40:00 +0000

    Check if remote "fork" exists

--- prune_repos.sh
+++ prune_repos.sh
@@ -3,9 +3,11 @@ START_PWD=$(pwd)
 for dir in src/*/
 do
     cd "$dir"
-    git fetch --prune fork
     git fetch --prune origin
-    git removed-branches --prune --force -r fork
     git removed-branches --prune --force -r origin
+    if git remote | grep -q '^fork$'; then
+      git fetch --prune fork
+      git removed-branches --prune --force -r fork
+    fi
     cd "$START_PWD"
 done
